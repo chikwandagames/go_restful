e09ca7de030daa4bbdc87b2644114b3e025dfe31
chikwandagames <chikwandagames@gmail.com>
2021-04-22 13:18:09 +0100

    ResponseWriter & Request

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
@@ -9,8 +11,22 @@ func main() {
 	// Handlers
 	// To run
 	// Curl -v localhost:9090
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
-		log.Println("Hello world")
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// ReadAll let you read from anything that implements io.Reader
+		// Body implements io.ReadClosr
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			// rw.WriteHeader(http.StatusBadRequest)
+			// rw.Write([]byte("Ooops!"))
+
+			// or
+			http.Error(rw, "Ooops!", http.StatusBadRequest)
+			return
+		}
+
+		log.Printf("Body contains: %s \n", d)
+		// Write data back to the user
+		fmt.Fprintf(rw, "Back to user: %s \n", d)
 	})
 
 	// To run, curl -v localhost:9090/goodbye
