eb1ec694b551c55e85376762ab7305e7d9ebf2b6
chikwandagames <chikwandagames@gmail.com>
2021-04-22 12:01:22 +0100

    basic handlers

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	// Handlers
+	// To run
+	// Curl -v localhost:9090
+	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+		log.Println("Hello world")
+	})
+
+	// To run, curl -v localhost:9090/goodbye
+	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
+		log.Println("Bye")
+	})
+
 	http.ListenAndServe(":9090", nil)
 }
